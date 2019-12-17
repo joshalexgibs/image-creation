20ea388813f186f8a383c0ed99360df9031fb3f9
unknown <joshalexgibs@gmail.com>
2019-12-16 21:10:10 -0800

    modified to allow user to input image size and  reformatted to reduce repeated code

--- Pixel.cs
+++ Pixel.cs
@@ -4,19 +4,52 @@ namespace System.Drawing
 {
   class Pixel
   {
+    private static byte[] _image;
+    private static int axisX;
+    private static int axisY;
+
     public static void Main(string[] args)
     {
-      if (args.Length == 0)
-        Console.WriteLine("Enter 'box' or 'circle'.");
-      else if (args[0] == "box")
+      if (args.Length < 3)
+        Console.WriteLine("Enter 'box' or 'circle' followed by width and length.");
+      else
       {
-        redBox();
-        //Console.WriteLine("Created redBox.png in current directory.");
+        bool pass_x = Int32.TryParse(args[1], out axisX);
+        bool pass_y = Int32.TryParse(args[2], out axisY);
+
+        if (!pass_x || !pass_y)
+          Console.WriteLine("Please enter a number for width and length.");
+        else if (args[0] == "box")
+        {
+          redBox();
+        }
+        else if (args[0] == "circle")
+        {
+          circle();
+        }
       }
-      else if (args[0] == "circle")
+    }
+    
+    static void sizeSet(int area)
+    {
+      _image = new byte[area];
+    }
+    
+    static void pixSet(int pos, byte blue, byte green, byte red, byte alpha)
+    {
+      _image[pos] = blue;
+      _image[pos + 1] = green;
+      _image[pos + 2] = red;
+      _image[pos + 3] = alpha;
+    }
+    
+    unsafe static void saveImg(string saveLoc)
+    {
+      fixed (byte* ptr = _image)
       {
-        circle();
-        //Console.WriteLine("Created redCircle.png in current directory.");
+        Bitmap image = new Bitmap(axisX, axisY, axisY * 4, Imaging.PixelFormat.Format32bppArgb, new IntPtr(ptr));
+        image.Save(@$"{saveLoc}");
+        Console.WriteLine("Created {0} in current directory.", saveLoc);
       }
     }
     
@@ -24,72 +57,47 @@ namespace System.Drawing
     {
       //  { b, g, r  , opacity }
       //  { 0, 0, 255, 255 }
-      byte[] redPix = new byte[400];
-      for(int x = 0; x < 400; x+=4)
+      int area = axisX * axisY * 4;
+      sizeSet(area);
+      for (int x = 0; x < area; x+=4)
       {
-        redPix[x] = 0;
-        redPix[x + 1] = 0;
-        redPix[x + 2] = 255;
-        redPix[x + 3] = 255;
+        pixSet(x, 0, 0, 255, 255);
       }
-      //fixed (byte* ptr = redPix)
-      //{
-      //              (width, height, stride * 4, format, pointer)
-      //  Bitmap image = new Bitmap(10, 10, 10 * 4, Imaging.PixelFormat.Format32bppRgb, new IntPtr(ptr));
-      //  image.Save(@"redBox.png");
-      //}
-      saveImg(redPix, 10, 10, 10, "redBox.png");
+      saveImg("redBox.png");
     }
     
     unsafe static void circle()
     {
-      byte[] redCir = new byte[40000];
-      for(double y = 0.0; y < 100; y++) // width: 20px
+      int area = axisX * axisY * 4;
+      int halfX = axisX / 2;
+      int halfY = axisY / 2;
+      int radius = (int) Math.Pow(halfX - 1, 2);
+      double upLim = axisX * (1 + ((axisX/1000.0) - 0.02));//1.08 at 100, 1 at 20
+      double lowLim = axisX * (-0.25 + (axisX/1500.0 - 0.13));//0.18 at 100, 0.25 at 20
+      Console.WriteLine("up: {0}, low: {1}", upLim, lowLim);
+      sizeSet(area);
+      for (int y = 0; y < axisY; y++)
       {
-        for(double x = 0.0; x < 100; x++) // height: 20px * 4 bytes
+        for(int x = 0; x < axisX; x++)
         {
-          double offset = (y * 400) + (x * 4);
-          double z = 1000 - (x - 50) * (x - 50) - (y - 50) * (y - 50);
-          if (z < 60 && z > 1)
+          int offset = (y * axisX * 4) + (x * 4);
+          float z = radius - (x - halfX + 1) * (x - halfX) - (y - halfY + 1) * (y - halfY);
+          if (z < upLim && z > lowLim)
           {
             //Console.WriteLine("Debug: {0}, {1}", x, y);
-            redCir[(int) offset] = 0;
-            redCir[(int) offset + 1] = 0;
-            redCir[(int) offset + 2] = 255;
-            redCir[(int) offset + 3] = 255;
+            pixSet(offset, 0, 0, 255, 255);
           }
-          else if ((x == 49 || x==50) && (y == 49 || y ==50))
+          else if ((x == halfX - 1 || x == halfX) && (y == halfY - 1 || y == halfY))
           {
-            redCir[(int)offset] = 255;
-            redCir[(int)offset + 1] = 0;
-            redCir[(int)offset + 2] = 0;
-            redCir[(int)offset + 3] = 255;
+            pixSet(offset, 255, 0, 0, 255);
           }
           else
           {
-            redCir[(int) offset] = 255;
-            redCir[(int) offset + 1] = 255;
-            redCir[(int) offset + 2] = 255;
-            redCir[(int) offset + 3] = 255;
+            pixSet(offset, 255, 255, 255, 255);
           }
         }
       }
-      //fixed (byte* ptr = redCir)
-      //{
-      //  Bitmap image = new Bitmap(100, 100, 100 * 4, Imaging.PixelFormat.Format32bppArgb, new IntPtr(ptr));
-      //  image.Save(@"redCircle.png");
-      //}
-      saveImg(redCir, 100, 100, 100, "redCircle.png");
-    }
-
-    unsafe static void saveImg(byte[] img, int x, int y, int stride, string saveLoc)
-    {
-      fixed (byte* ptr = img)
-      {
-        Bitmap image = new Bitmap(x, y, stride * 4, Imaging.PixelFormat.Format32bppArgb, new IntPtr(ptr));
-        image.Save(@$"{saveLoc}");
-        Console.WriteLine("Created {0} in current directory.", saveLoc);
-      }
+      saveImg("redCircle.png");
     }
   }
 }
